9f49ff37d2859cec75fe8e595a2f054000dc283b
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2025-11-08 22:22:41 +0200

    مسحت كل حاجة

--- GUI/Input.cpp
+++ GUI/Input.cpp
@@ -10,8 +10,7 @@ void Input::GetPointClicked(int &x, int &y)
 {
 	pWind->WaitMouseClick(x, y);	//Wait for mouse click
 }
-//Omar
-//gharbawy 
+
 string Input::GetSrting(Output *pOut)
 {
 	///TODO: Implement this Function
