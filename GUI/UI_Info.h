0f91d9e1a8edeb9f78f7fa3f95214cd7617e62a2
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2025-11-22 23:23:20 +0200

    added onst int y

--- GUI/UI_Info.h
+++ GUI/UI_Info.h
@@ -8,7 +8,8 @@ using namespace std;
 //User Interface information file.
 //This file contains info that is needed by Input and Output classes to
 //handle the user interface
-const int x = 50;
+const int x = 40;
+const int y = 40;
 __declspec(selectany) //This line to prevent "redefinition error"
 
 struct UI_Info	//User Interface Info.
@@ -17,7 +18,7 @@ struct UI_Info	//User Interface Info.
 	
 	static const int	width = 1000, height = 750,	//Window width and height
 						wx = 15 , wy = 15,			//Window starting coordinates
-						StatusBarHeight = 50,	//Status Bar Height
+						StatusBarHeight = 40,	//Status Bar Height
 						ToolBarHeight = 80,		//Tool Bar Height (distance from top of window to bottom line of toolbar)
 						ToolItemWidth = 80;		//Width of each item in toolbar menu
 
@@ -31,27 +32,27 @@ struct UI_Info	//User Interface Info.
 	//This should be calculated accurately because it will be used later to create connections between gates
 	//For now, we will assume that rect width = 50 and height = 50
 	static const int	AND2_Width = x,		//AND2 Gate Image default width
-						AND2_Height = x;		//AND2 Gate Image default height
+						AND2_Height = y;		//AND2 Gate Image default height
 	static const int	OR2_Width = x,		//OR2 Gate Image default width
-		OR2_Height = x;		//OR2 Gate Image default height
+		OR2_Height = y;		//OR2 Gate Image default height
 	static const int	XOR2_Width = x,		//XOR2 Gate Image default width
-		XOR2_Height = x;		//XOR2 Gate Image default height
+		XOR2_Height = y;		//XOR2 Gate Image default height
 	static const int	XNOR2_Width = x,		//XNOR2 Gate Image default width
-		XNOR2_Height = x;		//XNOR2 Gate Image default height
+		XNOR2_Height = y;		//XNOR2 Gate Image default height
 	static const int	AND3_Width = x,		//AND3 Gate Image default width
-		AND3_Height = x;		//AND3 Gate Image default height
+		AND3_Height = y;		//AND3 Gate Image default height
 	static const int	NOR3_Width = x,		//NOR3 Gate Image default width
-		NOR3_Height = x;		//NOR3 Gate Image default height
+		NOR3_Height = y;		//NOR3 Gate Image default height
 	static const int	XOR3_Width = x,		//XOR3 Gate Image default width
-		XOR3_Height = x;		//XOR3 Gate Image default height
+		XOR3_Height = y;		//XOR3 Gate Image default height
 	static const int    INV_Width = x,        //INV Gate Image default width
-		INV_Height = x;       //INV Gate Image default height
+		INV_Height = y;       //INV Gate Image default height
 	static const int    BUFF_Width = x,       //BUFF Gate Image default width
-		BUFF_Height = x;      //BUFF Gate Image default height
+		BUFF_Height = y;      //BUFF Gate Image default height
 	static const int    NAND2_Width = x,      //NAND2 Gate Image default width
-		NAND2_Height = x;     //NAND2 Gate Image default height
+		NAND2_Height = y;     //NAND2 Gate Image default height
 	static const int    NOR2_Width = x,       //NOR2 Gate Image default width
-		NOR2_Height = x;      //NOR2 Gate Image default height
+		NOR2_Height = y;      //NOR2 Gate Image default height
 
 
 	
