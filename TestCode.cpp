6f43fef562c2a7819f7ac593684ec2bb283346a7
Orashy23 <omarorashy8@gmail.com>
2025-11-23 13:49:03 +0200

    7agat malha4 lazma

--- TestCode.cpp
+++ TestCode.cpp
@@ -421,27 +421,6 @@ int main()
 		}
 		break;
 
-
-	/*	case ADD_CONNECTION:
-		{
-			pOut->PrintMsg("Action: add switch , Click anywhere");
-			pIn->GetPointClicked(x, y);
-			GraphicsInfo GfxInfo = {};
-			GfxInfo.x1 = x;
-			GfxInfo.y1 = y;
-			pOut->ChangeTitle("Connection Added at (" + to_string(x) + "," + to_string(y) + ")");
-			pOut->DrawConnection(GfxInfo, false);
-
-		}
-		break;*/
-
-
-
-
-
-
-
-
 		case SELECT:
 		{
 			pOut->PrintMsg("Action: a click on the Drawing area, Click anywhere");
