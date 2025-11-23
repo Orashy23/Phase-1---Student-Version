91007391d6a55649c3282c9510e0abe3cc1827c2
omaromar06-art <omar.omar06@eng-st.cu.edu.eg>
2025-11-23 22:30:56 +0200

    akher test

--- GUI/Output.cpp
+++ GUI/Output.cpp
@@ -24,7 +24,7 @@ Output::Output()
 	CreateDesignToolBar();	//Create the desgin toolbar
 	CreateStatusBar();		//Create Status bar
 	CreateLeftToolBar();    // Create the left tool bar
-// yarab teshta8al ha run //bismillah 
+// akher test enharda 
 }
 
 Input* Output::CreateInput() const
